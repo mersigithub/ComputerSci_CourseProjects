8ba629aa0de73e769dcf2bbec4bb9bae74bf6e5b
mersigithub <117305512+mersigithub@users.noreply.github.com>
2022-11-05 23:28:34 +0600

    Update minorMajor.h

--- minorMajor.h
+++ minorMajor.h
@@ -3,7 +3,8 @@
 using namespace std;
 // Keys Major and Minor class
 class Chord
-{public:
+{
+public:
     string CMajor = "C-D-E-F-G-A-B-C";
     string DMajor = "D-E-F#-G-A-B-C#-D";
     string EMajor = "E-F#-G#-A-B-C#-D#-E";
@@ -28,47 +29,50 @@ class Chord
         cout << "         PLEASE USE A CAPITAL LETTER AND SHARP '#' OR FLAT 'b' SIGN TO BUILD MAJOR SCALE.\n" << endl;
         cout << "                                !!!!! Do NOT MIX '#' AND 'b'\n" << endl;
         cout << "                                  Use sign (-) between Keys.\n" << endl;
-            cout << "Enter a Key: ";
-            cin >> Array;
-            cout << "Enter " << Array << " Key Major : ";
-            getline(cin, userAnswer); // will store the user input in userAnswer
-       // Now the checking part
+        cout << "Enter a Key: ";
+        cin >> Array;
+        cout << "Enter " << Array << " Key Major : ";
+        getline(cin, userAnswer); // will store the user input in userAnswer
+   // Now the checking part
 
-            while (userAnswer != CMajor && userAnswer != DMajor && userAnswer != EMajor && userAnswer != FMajor && userAnswer != GMajor && userAnswer != AMajor && userAnswer != BMajor) //this while will run util user enter wrong answer
-            {
-               // cout << "PLEASE TRY AGAIN!";
-                //cout << "\nPlease enter Major: ";
-                getline(cin, userAnswer);
-            }
-            cout << endl;
-            //as soon as user enter right answer while loop will be false and will come here
-            cout << "CONGRATULATIONS! Your Answer is Verified." << endl;
+        while (userAnswer != CMajor && userAnswer != DMajor && userAnswer != EMajor && userAnswer != FMajor && userAnswer != GMajor && userAnswer != AMajor && userAnswer != BMajor) //this while will run util user enter wrong answer
+        {
+            // cout << "PLEASE TRY AGAIN!";
+             //cout << "\nPlease enter Major: ";
+            getline(cin, userAnswer);
+        }
+        cout << endl;
+        //as soon as user enter right answer while loop will be false and will come here
+        cout << "CONGRATULATIONS! Your Answer is Verified." << endl;
     }
     // Keys Minor function doesn't return
-    void Minor() 
+    void Minor()
     {
-            string userAnswer = "";
-            char Array[] = { 'C', 'D', 'E', 'F', 'G', 'A', 'B' };
-            cout << "Enter a Key: ";
-            cin >> Array;
-            cout << endl;
-            cout << "PLEASE USE SHARP '#' OR FLAT 'b' SIGN TO BUILD MINOR SCALE.\n" << endl;
-            cout << "                                !!!!! Do NOT MIX '#' AND 'b'\n" << endl;
-            cout << "                                  Use sign (-) between Keys.\n" << endl;
-            cout << "Enter " << Array << " Key minor : ";
-            getline(cin, userAnswer); // will store the user input in userAnswer
+        string userAnswer = "";
+        char Array[] = { 'C', 'D', 'E', 'F', 'G', 'A', 'B' };
+        cout << "Enter a Key: ";
+        cin >> Array;
+        cout << endl;
+        cout << "PLEASE USE SHARP '#' OR FLAT 'b' SIGN TO BUILD MINOR SCALE.\n" << endl;
+        cout << "                                !!!!! Do NOT MIX '#' AND 'b'\n" << endl;
+        cout << "                                  Use sign (-) between Keys.\n" << endl;
+        cout << "Enter " << Array << " Key minor : ";
+        getline(cin, userAnswer); // will store the user input in userAnswer
 
-           // Now the checking part
+       // Now the checking part
 
-            while (userAnswer != Cminor && userAnswer != Dminor && userAnswer != Eminor && userAnswer != Fminor && userAnswer != Gminor && userAnswer != Aminor && userAnswer != Bminor) //this while will run util user enter wrong answer
-            {
-                //cout << "PLEASE TRY AGAIN!";
-                //cout << "\nPlease enter minor: ";
-                getline(cin, userAnswer);
-            }
-            cout << endl;
-            //as soon as user enter right answer while loop will be false and will come here
-            cout << "CONGRATULATIONS! Your Answer is Verified." << endl;
+        while (userAnswer != Cminor && userAnswer != Dminor && userAnswer != Eminor && userAnswer != Fminor && userAnswer != Gminor && userAnswer != Aminor && userAnswer != Bminor) //this while will run util user enter wrong answer
+        {
+            //cout << "PLEASE TRY AGAIN!";
+            //cout << "\nPlease enter minor: ";
+            getline(cin, userAnswer);
+        }
+        cout << endl;
+        //as soon as user enter right answer while loop will be false and will come here
+        cout << "CONGRATULATIONS! Your Answer is Verified." << endl;
     }
 };
+
+
+
        
